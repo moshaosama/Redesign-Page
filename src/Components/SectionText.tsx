9187a059ac460d1e606c3509cbd7530699b2a917
moshaosama <mohamedOSFekry@gmail.com>
2025-06-07 22:20:44 +0300

    modified: SectionText.tsx

--- src/Components/SectionText.tsx
+++ src/Components/SectionText.tsx
@@ -1,6 +1,15 @@
+import clsx from "clsx";
+import { useGetTheme } from "../Context/ToggleLightContext";
+
 const SectionText = () => {
+  const { theme } = useGetTheme();
   return (
-    <div className="text-center mt-10 text-white">
+    <div
+      className={clsx(
+        "text-center mt-10 ",
+        theme === "dark" ? "text-white" : " text-black"
+      )}
+    >
       <h1 className="text-3xl font-bold">Choose Your Skip Size </h1>
       <p className="text-xl mt-5">
         Select the skip size that best suits your needs
