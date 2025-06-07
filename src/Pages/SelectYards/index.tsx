dc2c2633887b794dcad9d438d50b3dd017a1fac8
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 01:14:15 +0300

    modified: SelectYards/index.tsx

--- src/Pages/SelectYards/index.tsx
+++ src/Pages/SelectYards/index.tsx
@@ -1,3 +1,4 @@
+import DetailsYard from "../../Components/DetailsYard";
 import Header from "../../Components/Header";
 import SectionText from "../../Components/SectionText";
 import YardsRender from "../../Components/YardsRender";
@@ -5,10 +6,11 @@ import YardsRender from "../../Components/YardsRender";
 const SelectYards = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center ">
+      <div className="flex flex-col items-center justify-center relative">
         <Header />
         <SectionText />
         <YardsRender />
+        <DetailsYard />
       </div>
     </>
   );
