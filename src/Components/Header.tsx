f7e7b0e9424785768e61d479813310282167f6ca
moshaosama <mohamedOSFekry@gmail.com>
2025-06-07 22:20:44 +0300

    modified: Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,44 @@
+import { CiLight } from "react-icons/ci";
 import { HeaderData } from "../Data/HeaderData";
 
 import type { HeaderType } from "../Types/HeaderType";
+import { useGetTheme } from "../Context/ToggleLightContext";
+import clsx from "clsx";
 
 const Header = () => {
+  const { ToggleTheme, theme } = useGetTheme();
   return (
     <div className="flex gap-4 mt-10">
       {HeaderData.map((el: HeaderType) => {
         return (
-          <div key={el.id} className="text-gray-300 flex gap-3  items-center">
+          <div
+            key={el.id}
+            className={clsx(
+              " flex gap-3  items-center",
+              theme === "dark" ? "text-gray-300" : "text-black"
+            )}
+          >
             <div className="flex items-center gap-1">
               <i>{el.Icon}</i>
               <h1 className="text-md font-semibold">{el.Title}</h1>
             </div>
-            <hr className="h-[0.1px] bg-gray-300 border-none w-[4pc]" />
+            <hr
+              className={clsx(
+                "h-[0.1px]  border-none w-[4pc]",
+                theme === "dark" ? "bg-gray-300" : "bg-black"
+              )}
+            />
           </div>
         );
       })}
+      <div className="">
+        <CiLight
+          color="white"
+          size={40}
+          cursor={"pointer"}
+          onClick={ToggleTheme}
+        />
+      </div>
     </div>
   );
 };
