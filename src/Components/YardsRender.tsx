9de573e6f7bb2be53e3838639fec2041178cb095
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 01:14:15 +0300

    modified: YardsRender.tsx

--- src/Components/YardsRender.tsx
+++ src/Components/YardsRender.tsx
@@ -5,26 +5,28 @@ import { FaCheckCircle, FaRegCircle } from "react-icons/fa";
 import { useGetTheme } from "../Context/ToggleLightContext";
 
 const YardsRender = () => {
-  const { Yards } = useGetData();
+  const { Yards, DelayData } = useGetData();
   const { theme } = useGetTheme();
   const { OnSubmit, selectedIndex } = useSelectData();
   return (
     <>
       <div
         className={clsx(
-          "mt-10 flex flex-col gap-10",
-          theme === "dark" ? "text-white" : "text-black"
+          "mt-10 mb-28 max-sm:mb-56 flex flex-col gap-10 transition-all duration-1000 ",
+          theme === "dark" ? "text-white" : "text-black",
+          DelayData ? "ml-0 opacity-100" : "ml-96 opacity-0"
         )}
       >
         {Array.isArray(Yards?.yards) ? (
           Yards?.yards?.map((yard) => (
             <div
+              key={yard.id}
               id="CardData"
               onClick={() => OnSubmit(yard.id)}
               className={clsx(
-                "  shadow-lg flex max-sm:flex-col justify-between items-center shadow-gray-700 w-[75pc] max-sm:w-96 rounded-lg cursor-pointer hover:scale-105 transition-all duration-700 ease-in-out",
+                "shadow-lg flex max-sm:flex-col justify-between items-center shadow-gray-700 w-[75pc] max-xl:w-[55pc] max-sm:w-[22pc] max-lg:w-[45pc] rounded-lg cursor-pointer hover:scale-105 transition-all duration-700 ease-in-out",
                 selectedIndex === yard.id
-                  ? "border-[#0037c1] border-2"
+                  ? "border-[#0037c1] border-4"
                   : "border-1 border-gray-400"
               )}
             >
@@ -35,16 +37,18 @@ const YardsRender = () => {
                   className="w-60 rounded-l-lg max-sm:w-full max-sm:rounded-lg"
                 />
                 <div className="max-sm:flex max-sm:mt-5 max-sm:justify-between max-sm:items-center max-sm:gap-10">
-                  <div className="ml-10">
+                  <div className="ml-10 max-sm:ml-0">
                     <h1 className="font-bold text-2xl max-sm:text-xl">
                       {yard.size} Yard Size
                     </h1>
-                    <p>{yard.hire_period_days} day hire period</p>
+                    <p className="max-sm:text-[13px]">
+                      {yard.hire_period_days} day hire period
+                    </p>
                     <p className="text-[#0037c1] mt-10 text-4xl max-sm:text-2xl max-sm:mt-0 font-bold">
                       £ {yard.price_before_vat}
                     </p>
                   </div>
-                  <div className="mr-10 sm:hidden">
+                  <div className="mr-10 max-sm:mr-0 sm:hidden">
                     <button
                       className={clsx(
                         " text-xl flex  items-center justify-center gap-2 w-32 py-2 rounded-xl  font-bold cursor-pointer",
