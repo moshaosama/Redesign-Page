73eb0f53cddd5a3aff8a215e14a5d7ff596741b0
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 01:14:15 +0300

    modified: useSelectData.tsx

--- src/Hooks/useSelectData.tsx
+++ src/Hooks/useSelectData.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export const useSelectData = () => {
-  const [selectedIndex, setSelectedIndex] = useState<null | number>(null);
+  const [selectedIndex, setSelectedIndex] = useState<null | number>(17933);
+  const Naviagate = useNavigate();
 
   const OnSubmit = (id: number) => {
     setSelectedIndex(id);
+    Naviagate(`${id}`);
   };
 
   return { selectedIndex, OnSubmit };
