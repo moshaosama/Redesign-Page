ba20ca14548536c2f22f13be54aede7a348ab336
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 01:14:15 +0300

    modified: useGetData.ts

--- src/Hooks/useGetData.ts
+++ src/Hooks/useGetData.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { type AppDispatch, type RootState } from "../Store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchGetYards } from "../Actions/fetchGetYards";
 import type { GetDataYardsSlice } from "../Types/YardsType";
 
@@ -8,12 +8,17 @@ const useGetData = () => {
   const Yards = useSelector(
     (state: RootState) => state.Yards
   ) as GetDataYardsSlice;
+  const [DelayData, setDalayData] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchGetYards());
   }, [dispatch]);
 
-  return { Yards };
+  useEffect(() => {
+    setDalayData(true);
+  }, [DelayData]);
+
+  return { Yards, DelayData };
 };
 
 export default useGetData;
